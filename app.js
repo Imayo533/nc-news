8036ea6a193d26349cebe291a45492df00d914bd
iam53382@gmail.com <iam53382@gmail.com>
2024-02-21 09:36:02 +0000

    nc_news_task 4 completed

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const app = express()
-const {getTopics, getApi} = require("./controller")
+const {getTopics, getApi, getArticleById} = require("./controller")
+app.use(express.json())
 
 app.get("/api/topics", getTopics)
 
 app.get("/api", getApi)
 
+app.get("/api/articles/:article_id", getArticleById)
+
 app.all("/api/*", (request, response, next)=>{
     response.status(404).send({msg:"Not found!"})
     next()
+})
+
+app.use((err, request, response, next)=>{
+    if(err){
+    response.status(400).send({msg: "Bad request"})
+    }
 })
 
 app.use((err, request, response, next)=>{
--- model.js
+++ model.js
@@ -7,5 +7,10 @@ exports.allTopics = () => {
         return result.rows
     })
 }
-
+exports.selectArticleById = (article_id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    .then(({rows})=>{
+        return rows[0]
+    })
+}
 
